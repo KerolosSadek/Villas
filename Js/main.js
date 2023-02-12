95c49e44d1f6857667482eaa6714074e393c52da
Kerolos Sadek <kokosadek55@gmail.com>
2023-02-12 15:59:09 +0200

    First Version

---
+++ Js/main.js
@@ -0,0 +1,13 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+window.setTimeout(openForm, 4000);
+
+// function hide(){
+//   document.getElementsByClassName(sproket).style.display = "none"
+// }
+
+// window.setTimeout(hide, 10000)
